d161820c7a2475606c599654a9709ab43815c77e
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-23 19:37:46 -0700

    setting up babel loader and plugins in webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -18,34 +18,34 @@ const config = {
     new WebpackPwaManifest({
       fingerprints: false,
       inject: false,
-      name: 'Images App',
-      short_name: 'Images App',
-      description: 'An application for images',
+      name: 'Budget Tracker App',
+      short_name: 'budget App',
+      description: 'An application for keeping track of budgeting',
       background_color: '#01579b',
       theme_color: '#ffffff',
       start_url: '/',
       icons: [
         {
-          //   src: path.resolve('public/assets/images/icons/icon-192x192.png'),
-          //   sizes: [96, 128, 192, 256, 384, 512],
-          //   destination: path.join('assets', 'icons'),
+          src: path.resolve('public/icons/icon-192x192.png'),
+          sizes: [192, 512],
+          destination: path.join('assets', 'icons'),
         },
       ],
     }),
   ],
-  //   module: {
-  //     rules: [
-  //       {
-  //         test: /\.m?js$/,
-  //         exclude: /(node_modules)/,
-  //         use: {
-  //           loader: 'babel-loader',
-  //           options: {
-  //             presets: ['@babel/preset-env'],
-  //           },
-  //         },
-  //       },
-  //     ],
-  //   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
 module.exports = config;
